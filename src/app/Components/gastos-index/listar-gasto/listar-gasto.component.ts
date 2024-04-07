67b62d3141f7557e11ac5f4593ab7c6c048cc4fc
Hector <hectordb19@gmail.com>
2024-04-07 12:32:21 -0400

    x

--- src/app/Components/gastos-index/listar-gasto/listar-gasto.component.ts
+++ src/app/Components/gastos-index/listar-gasto/listar-gasto.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PresupuestoService } from '../../../services/presupuesto.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-listar-gasto',
@@ -9,9 +10,27 @@ import { PresupuestoService } from '../../../services/presupuesto.service';
   styleUrl: './listar-gasto.component.css',
 })
 export class ListarGastoComponent {
+  subscription: Subscription;
+  presupuesto: number;
+  restante: number;
+  listGastos: any[] = [];
+
   constructor(private _presupuestoService: PresupuestoService) {
-    this._presupuestoService.getGastos().subscribe((data) => {
-      console.log(data);
-    });
+    this.presupuesto = 0;
+    this.restante = 0;
+    this.subscription = this._presupuestoService
+      .getGastos()
+      .subscribe((data) => {
+        this.restante = this.restante - data.cantidad;
+        this.listGastos.push(data);
+      });
+  }
+  ngOnInit(): void {
+    this.presupuesto = this._presupuestoService.presupuesto;
+    this.restante = this._presupuestoService.restante;
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe;
   }
 }
